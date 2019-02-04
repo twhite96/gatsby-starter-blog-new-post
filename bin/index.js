bad4ae9b5f92952d83ea4e82972343ab94f3af28
Tiffany White <hey@tiffanyrwhite.com>
2019-02-03 22:43:29 -0500

    Finished but need docs

--- bin/index.js
+++ bin/index.js
@@ -1,30 +1,34 @@
+#!/usr/bin/env node
+
 const fs = require('fs');
 const slugify = require('slug');
 const dateFns = require('date-fns');
 const title = process.argv[2];
+const shell = require("shelljs");
+
+shell.exec("new post");
+
 if (!title) {
- throw 'a title is required!';
+  throw 'a title is required!';
 }
 const slug = slugify(title.toLowerCase());
 const date = dateFns.format(new Date(), 'YYYY-MM-DD');
 const dir = `./src/pages/${slug}`;
-
 if (!dir) {
   throw 'this directory does not exist!';
 }
-
 fs.writeFileSync(
- `${dir}.md`,
- `---
-date: ${date}
-title: "${title}"
-author:
-spoiler:
----`,
- function(err) {
- if (err) {
- return console.log(err)
- }
- console.log(`${title} was created!`)
- },
+  `${dir}.md`,
+  `---
+ date: ${date}
+ title: "${title}"
+ author:
+ spoiler:
+ ---`,
+  function(err) {
+    if (err) {
+      return console.log(err);
+    }
+    console.log(`${title} was created!`);
+  }
 );
